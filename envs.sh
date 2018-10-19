3dfb4cf6df108406100b64c759ef0151b49ae909
Ashley Handscomb Retallack <ashley@solidangle.com>
2018-10-19 13:36:43 +0100

    added more python options

---
+++ envs.sh
@@ -0,0 +1,63 @@
+
+if [ -z $HOUDINI_VERSION ];then
+    export HOUDINI_VERSION=16.5.496
+fi
+
+if [ -z $HTOA_VERSION ];then
+    export HTOA_VERSION=3.1.1
+fi
+
+if [ -z $MAYA_VERSION ];then
+    export MAYA_VERSION=2018
+fi
+
+if [ -z $MTOA_VERSION ];then
+    export MTOA_VERSION=3.1.0.1
+fi
+
+if [ -z $ARNOLD_VERSION ];then
+    export ARNOLD_VERSION=5.2.0.1
+fi
+
+if [ -z $ALSHADERS_VERSION ];then
+    export ALSHADERS_VERSION=2.0.0b2
+fi
+
+if [ -z $ALSHADERS_LEGACY_VERSION ];then
+    export ALSHADERS_LEGACY_VERSION=1.0.0rc20
+fi
+
+if [ -z $ALEMBIC_VERSION ];then
+    export ALEMBIC_VERSION=1.7.4
+fi
+
+if [ -z $USD_VERSION ];then
+    export USD_VERSION=0.8.3
+fi
+
+if [ -z $PYTHON_VERSION ];then
+    export PYTHON_VERSION=2.7.14
+fi
+
+if [ -z $JSNOISE_VERSION ];then
+    export JSNOISE_VERSION=02102017
+fi
+
+if [ -z $BBALEMBIC_VERSION ];then
+    export BBALEMBIC_VERSION=2.0.0
+fi
+
+if [ -z $YETI_VERSION ];then
+    export YETI_VERSION=2.2.6
+fi
+
+if [ -z $GOLAEM_VERSION ];then
+    export GOLAEM_VERSION=6.3
+fi
+
+if [ -z $WALTER_VERSION ];then
+    export WALTER_VERSION=1.0.1
+fi
+
+export SW=/software
+export SA_ROOT=$SW/solidangle
